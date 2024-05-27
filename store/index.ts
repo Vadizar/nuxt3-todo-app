0140fa033df42ad0c626f762fe07bd925324b55d
Vadizar <vadizar@gmail.com>
2024-05-27 17:15:59 +0300

    Revert "Remove unused pinia"
    This reverts commit fedf84ff9a24560bc46ac82bbad6ae6c44c6cd29.

---
+++ store/index.ts
@@ -0,0 +1,26 @@
+import { defineStore } from 'pinia'
+
+export const useUserStore = defineStore('user', {
+    state: () => ({
+        users: [],
+        page: 1,
+        searchQuery: ''
+    }),
+    actions: {
+        async fetchUsers() {
+            const response = await fetch('https://jsonplaceholder.typicode.com/users')
+            const data = await response.json()
+            this.users = data
+        }
+    },
+    getters: {
+        filteredUsers: (state) => {
+            return state.users.filter(user =>
+                user.name.toLowerCase().includes(state.searchQuery.toLowerCase())
+            ).slice((state.page - 1) * 5, state.page * 5)
+        },
+        hasMoreUsers: (state) => {
+            return state.users.length > state.page * 5
+        }
+    }
+})
