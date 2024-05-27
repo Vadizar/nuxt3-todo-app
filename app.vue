d6543f45fe9c0db02622a65436e7006ae5cec196
Vadizar <vadizar@gmail.com>
2024-05-27 16:05:45 +0300

    Add sass

--- app.vue
+++ app.vue
@@ -1,36 +1,41 @@
 <template>
-    <div>
-        <header>
-            <nav>
-                <NuxtLink to="/">
-                    Home
-                </NuxtLink>
-                <NuxtLink to="/users">
-                    Users
-                </NuxtLink>
-            </nav>
-        </header>
-        <main>
-            <NuxtPage />
-        </main>
-    </div>
+    <header>
+        <nav>
+            <NuxtLink to="/">
+                Home
+            </NuxtLink>
+            <NuxtLink to="/users">
+                Users
+            </NuxtLink>
+        </nav>
+    </header>
+    <main>
+        <NuxtPage />
+    </main>
 </template>
 
 <script lang="ts" setup>
 </script>
 
-<style scoped>
-/* Добавьте свои стили здесь */
-header {
-  background-color: #333;
-  color: white;
-  padding: 1rem;
-}
-nav {
-  display: flex;
-  gap: 1rem;
-}
-main {
-  padding: 1rem;
-}
+<style lang="sass">
+body
+    font-family: Arial, sans-serif
+    margin: 0
+    padding: 0
+    box-sizing: border-box
+
+header
+    background-color: #333
+    color: white
+    padding: 1rem
+
+nav
+    display: flex
+    gap: 1rem
+
+main
+    padding: 1rem
+
+button
+    cursor: pointer
 </style>
